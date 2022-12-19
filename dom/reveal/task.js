544fdade96998b59ad210c838a8d03912c2a2d24
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2022-12-19 22:34:50 +0300

    new task

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,8 +2,12 @@ const reveals = document.getElementsByClassName("reveal");
 
 window.addEventListener("scroll", () => {
    for (const reveal of reveals) {
-      if (window.innerHeight > reveal.getBoundingClientRect().top) {
+      const { innerHeight } = window;
+      const { top } = reveal.getBoundingClientRect();
+      if (top < innerHeight && top > 0) {
          reveal.classList.add("reveal_active");
-      }
+      } else {
+         reveal.classList.remove("reveal_active");
+      }  
    }
 })
