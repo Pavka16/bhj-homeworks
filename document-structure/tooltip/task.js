6586c7a64475049678231996d63a86873da7b123
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2023-01-05 22:30:57 +0300

    new change task

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -8,10 +8,34 @@ for (const link of tooltipLinks) {
 
    link.addEventListener("click", function(e) {
       e.preventDefault();
+      if (tooltip.classList.contains("tooltip_active")) {
+         tooltip.classList.remove("tooltip_active");
+         return;
+      }
+
       const prevTooltip = document.querySelector(".tooltip_active");
       if (prevTooltip) 
          prevTooltip.classList.remove("tooltip_active");
+         const linkRect = this.getBoundingClientRect();
          tooltip.classList.add("tooltip_active");
-      
+         const tooltipRect = tooltip.getBoundingClientRect();
+
+      switch (tooltip.dataset.position) {
+         case "left":
+            tooltip.style.top = linkRect.top + "px";
+            tooltip.style.left = (linkRect.left + tooltipRect.left - tooltipRect.right) + "px";
+            break;
+         case "right":
+            tooltip.style.top = linkRect.top + "px";
+            tooltip.style.left = linkRect.right + "px";
+            break;
+         case "top":
+            tooltip.style.top = (linkRect.top + tooltipRect.top - tooltipRect.bottom) + "px";
+            tooltip.style.left = linkRect.left + "px";
+            break;
+         default:
+            tooltip.style.top = linkRect.bottom + "px";
+            tooltip.style.left = linkRect.left + "px";
+      }
    })
 }
